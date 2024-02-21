e62c7244158222a4ed9343827250ac475b7d2db1
[neimanis] <[ernestsneimanis]>
2024-02-21 10:10:29 +0100

    container sizes

--- src/components/rental-details/RentalDetails.vue
+++ src/components/rental-details/RentalDetails.vue
@@ -6,7 +6,7 @@ import UniqueLinkCopy from "./UniqueLinkCopy.vue"
 
 </script>
 <template>
-    <div class="relative min-h-52 w-[90%] z-[10] max-w-[1050px] min-w-[800px] bg-indigo-950 h-52 rounded-3xl pt-7 overflow-hidden">
+    <div class="relative min-h-52 w-[100%] z-[10] max-w-[1100px] min-w-[900px] bg-indigo-950 h-52 rounded-3xl pt-7 overflow-hidden">
 
         <div class="absolute  z-[15] bottom-0 right-0 w-52 h-52 translate-x-32 translate-y-24 rounded-full scale-150 border border-white">
             <div class="absolute top-0 left-0 border rounded-full translate-x-5 translate-y-4 border-white w-6 h-6">
@@ -14,6 +14,7 @@ import UniqueLinkCopy from "./UniqueLinkCopy.vue"
             </div>
         </div>
 
+        
         <div class="relative z-[25]"> 
           
             <div class="flex flex-col justify-center items-center gap-2 ">
