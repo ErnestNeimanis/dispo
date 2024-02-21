7a738d9d515822189144745d5845558ebae914f3
[neimanis] <[ernestsneimanis]>
2024-02-21 09:08:47 +0100

    shadow correction

--- src/components/rental-profile/RentalProfile.vue
+++ src/components/rental-profile/RentalProfile.vue
@@ -8,7 +8,7 @@ import LocationDetails from './LocationDetails.vue';
 import Map from './Map.vue';
 </script>
 <template>
-    <div class="flex justify-center flex-wrap h-full min-w-[400px]  px-12 py-8 pt-10 bg-white shadow-md shadow-black rounded-xl ">
+    <div class="flex justify-center flex-wrap h-full min-w-[400px]  px-12 py-8 pt-10 bg-white shadow-md shadow-gray-400 rounded-xl ">
         <div class="flex flex-col flex-wrap w-1/3 min-w-[300px]">
             <div class="flex justify-center  gap-8 ">
                 <UserProfile class="border-r-2 pr-8" />
