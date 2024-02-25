02fbd5d661bec42805afdedb92f32fc56c67d580
[neimanis] <[ernestsneimanis]>
2024-02-25 21:28:01 +0100

    main element and header margins

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,27 +1,30 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { useWindowSize } from '@/window';
 import NavBar from "./NavBar.vue"
 import Logo from "@/assets/images/dispotravel_logo1.png"
 
 
-
+const {largeWindow,mediumWindow,smallWindow} = useWindowSize()
 
 
 
 
 </script>
 <template>
-    <div class="flex justify-between items-center  px-16 h-32  min-w-[700px]">
-        <div class="flex items-center justify-center h-full gap-6">
+    <div
+     :class="{'mx-32':largeWindow,'mx-8':mediumWindow,'mx-2':smallWindow}" 
+    class="flex justify-between items-center   h-32  min-w-[700px">
+        <div class="flex items-center  h-full gap-6">
 
-            <div class="flex items-center justify-center ">
+            <div class="flex items-center  ">
                 <RouterLink to="/">
                     <img class=" min-w-[150px] h-[49px] hover:cursor-pointer" :src="Logo" alt="">
                 </RouterLink>
             </div>
 
-            <div class="flex items-center justify-center gap-4 px-4 ">
+            <div v-if="largeWindow" class="flex items-center justify-center gap-4 px-4 ">
                 <div class="flex gap-1 text-sm font-bold text-gray-500 ">
                     <span>EUR</span>
                     <span>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-  import RentalDetails from "@/components/rental-details/RentalDetails.vue"
-    import RentalProfile from "@/components/rental-profile/RentalProfile.vue"
+import RentalDetails from "@/components/rental-details/RentalDetails.vue"
+import RentalProfile from "@/components/rental-profile/RentalProfile.vue"
 import Footer from "@/components/Footer.vue";
+import { useWindowSize } from "@/window";
+const {largeWindow,mediumWindow,smallWindow} = useWindowSize()
 </script>
 
 <template>
+  <div 
+  :class="{'mx-32':largeWindow,'mx-8':mediumWindow,'mx-2':smallWindow}"
+  class="relative flex flex-col items-center  mt-8 min-w-[400px] bg-white h-3/4 rounded-2xl">
 
-  <div class="relative flex flex-col items-center mx-32 mt-8 min-w-[400px] bg-white h-3/4 rounded-2xl">
-  
     <RentalDetails class="z-0 -translate-y-8" />
-    <RentalProfile class="z-30 -translate-y-24 "/>
-    
+    <RentalProfile class="z-30 -translate-y-24 " />
+
   </div>
-
-
-  
 </template>
